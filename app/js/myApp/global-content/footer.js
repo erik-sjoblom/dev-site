b52028062cdd6035dbd2f85795c9eb27e3ceac04
Erik Sjoblom <erik.h.sjoblom@gmail.com>
2016-11-18 11:40:31 -0500

    fixed position footer on short pages

--- app/js/myApp/global-content/footer.js
+++ app/js/myApp/global-content/footer.js
@@ -8,6 +8,28 @@
       return {
         restrict: 'E',
         templateUrl: '/partials/global-content/footer.html',
+        link: function(scope, elem, attrs) {
+            function checkContentHeight() {
+              jQuery(function($) {
+                var contentHeight = $('.main-content').outerHeight() + $('.site-footer').outerHeight();
+                if(window.innerHeight >= contentHeight) {
+                  $('.site-footer').addClass('fixed');
+                } else {
+                  $('.site-footer').removeClass('fixed');
+                }
+              });
+            }
+            jQuery(function($) {
+              checkContentHeight();
+              $(window).on('resize', checkContentHeight);
+            });
+            scope.$watch(function() { 
+              return jQuery(document).height();
+            }, function onHeightChange(newValue, oldValue) {
+              checkContentHeight();
+            });
+            scope.theDate = new Date().toJSON().slice(0,10);
+        }
       }
     });
   
